4618a841ecef245f81a6799c923a5a8085050544
RasussRogue <hechadi.mehdi.pro@gmail.com>
2021-03-22 16:26:24 +0100

    Added a static Champion Stats component

--- src/components/champion.tsx
+++ src/components/champion.tsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
-
-import {
-    Avatar,
-    Button,
-    Grid,
-    List,
-    ListItem,
-    ListItemAvatar,
-    ListItemText
-    // @ts-ignore
-} from "@material-ui/core";
+// @ts-ignore
+import { Avatar, Button, Grid, List, ListItem, ListItemAvatar, ListItemText} from "@material-ui/core";
 import {FC, useEffect, useState} from "react";
+// @ts-ignore
+import {mdiSword, mdiFire, mdiPlusOutline, mdiFlash, mdiShield, mdiAxe, mdiCircleSlice8, mdiRunFast} from '@mdi/js';
+// @ts-ignore
+import Icon from '@mdi/react'
 
 interface Data {
     data:any
@@ -32,10 +27,36 @@ const Champion: FC<ChampionProps> = ({champion}) => {
         <div>
             <ListItem key={champion.name}>
                 <ListItemAvatar>
-                    <Avatar alt='Aatrox' src="http://ddragon.leagueoflegends.com/cdn/11.6.1/img/champion/Aatrox.png"></Avatar>
+                    <Avatar alt='Aatrox' src="http://ddragon.leagueoflegends.com/cdn/11.6.1/img/champion/Aatrox.png"/>
                 </ListItemAvatar>
                 <ListItemText primary='Aatrox' secondary='Fighter/Tank'/>
             </ListItem>
+            <Grid style={{fontFamily: 'Roboto'}} container spacing={3}>
+                <Grid item xs={6}>
+                    <Icon style={{verticalAlign:'bottom'}} path={mdiFire} size={1}/> 0
+                </Grid>
+                <Grid item xs={6}>
+                    <Icon style={{verticalAlign:'bottom'}} path={mdiSword} size={1}/> 0.651
+                </Grid>
+                <Grid item xs={6}>
+                    <Icon style={{verticalAlign:'bottom'}} path={mdiPlusOutline} size={1}/> 580
+                </Grid>
+                <Grid item xs={6}>
+                    <Icon style={{verticalAlign:'bottom'}} path={mdiFlash} size={1}/> 0
+                </Grid>
+                <Grid item xs={6}>
+                    <Icon style={{verticalAlign:'bottom'}} path={mdiShield} size={1}/> 38
+                </Grid>
+                <Grid item xs={6}>
+                    <Icon style={{verticalAlign:'bottom'}} path={mdiAxe} size={1}/> 60
+                </Grid>
+                <Grid item xs={6}>
+                    <Icon style={{verticalAlign:'bottom'}} path={mdiCircleSlice8} size={1}/> 32
+                </Grid>
+                <Grid item xs={6}>
+                    <Icon style={{verticalAlign:'bottom'}} path={mdiRunFast} size={1}/> 580
+                </Grid>
+            </Grid>
         </div>
     )
 }
