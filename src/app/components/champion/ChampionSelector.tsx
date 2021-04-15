2903853a55b1ed3c333b6b2a77252dc61e56fc0f
RasussRogue <hechadi.mehdi.pro@gmail.com>
2021-04-15 17:28:14 +0200

    (SC-15) Added the Axios request to DataDragon to get te champion's list

--- src/app/components/champion/ChampionSelector.tsx
+++ src/app/components/champion/ChampionSelector.tsx
@@ -7,24 +7,28 @@ import axios from 'axios';
 
 export const ChampionSelector = () => {
     const [champion, setChampion] = useState<Champion>({name: 'Aatrox', title: 'the Destroyer', blurb: 'Placeholder'},);
-
-    const championsList = [
-        {name: 'Aatrox', title: 'the Destroyer', blurb: 'Placeholder'},
-        {name: 'Gangplank', title: 'the Pirate', blurb: 'Placeholder'},
-        {name: 'Yorick', title: 'the Grave digger', blurb: 'Placeholder'},
-        {name: 'Ahri', title: 'the Cat', blurb: 'Placeholder'},
-    ]
+    const [championsList, setChampionsList] = useState<Champion[]>([]);
 
     useEffect(() => {
+        if (championsList.length == 0) fetchChampionList()
         loadChampion();
     }, [])
 
+    function fetchChampionList() {
+        axios.get(`https://ddragon.leagueoflegends.com/cdn/11.8.1/data/en_US/champion.json`, {}).then(response => {
+            const payload = response.data as Data
+            const champs = Object.values(payload.data) as Champion[]
+            console.log(champs)
+            setChampionsList(champs)
+        })
+    }
+
     function loadChampion() {
         axios.get(`https://ddragon.leagueoflegends.com/cdn/11.6.1/data/en_US/champion/Aatrox.json`, {}).then(response => {
-            const payload = response as Data
+            const payload = response.data as Data
             const champs = Object.values(payload.data) as Champion[]
             const champion = champs[0]
-            //setChampion(champion as Champion)
+            setChampion(champion as Champion)
         })
     }
 
