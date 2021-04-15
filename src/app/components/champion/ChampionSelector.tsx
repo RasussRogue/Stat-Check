54820d818dd7ab7eaf4583472423a6810c610fde
RasussRogue <hechadi.mehdi.pro@gmail.com>
2021-04-15 16:11:30 +0200

    (SC-15) Now uses Axios for API calls

--- src/app/components/champion/ChampionSelector.tsx
+++ src/app/components/champion/ChampionSelector.tsx
@@ -1,17 +1,18 @@
 import React, {useCallback} from "react";
-import { useEffect, useState } from "react";
-import { Champion, Data } from "../model/models";
-import { ChampionView } from "./ChampionView";
+import {useEffect, useState} from "react";
+import {Champion, Data} from "../model/models";
+import {ChampionView} from "./ChampionView";
+import axios from 'axios';
 
 
 export const ChampionSelector = () => {
-    const [champion, setChampion] = useState<Champion>({name: 'Aatrox', title: 'the Destroyer', blurb : 'Placeholder'},);
+    const [champion, setChampion] = useState<Champion>({name: 'Aatrox', title: 'the Destroyer', blurb: 'Placeholder'},);
 
     const championsList = [
-        {name: 'Aatrox', title: 'the Destroyer', blurb : 'Placeholder'},
-        {name: 'Gangplank', title: 'the Pirate', blurb : 'Placeholder'},
-        {name: 'Yorick', title: 'the Grave digger', blurb : 'Placeholder'},
-        {name: 'Ahri', title: 'the Cat', blurb : 'Placeholder'},
+        {name: 'Aatrox', title: 'the Destroyer', blurb: 'Placeholder'},
+        {name: 'Gangplank', title: 'the Pirate', blurb: 'Placeholder'},
+        {name: 'Yorick', title: 'the Grave digger', blurb: 'Placeholder'},
+        {name: 'Ahri', title: 'the Cat', blurb: 'Placeholder'},
     ]
 
     useEffect(() => {
@@ -19,15 +20,12 @@ export const ChampionSelector = () => {
     }, [])
 
     function loadChampion() {
-        fetch(`http://ddragon.leagueoflegends.com/cdn/11.6.1/data/en_US/champion/Aatrox.json`, {
-            method: 'GET',
-        }).then(response => response.json())
-            .then(response => {
-                const payload  = response as Data
-                const champs = Object.values(payload.data) as Champion[]
-                const champion = champs[0]
-                setChampion(champion as Champion)
-            })
+        axios.get(`https://ddragon.leagueoflegends.com/cdn/11.6.1/data/en_US/champion/Aatrox.json`, {}).then(response => {
+            const payload = response as Data
+            const champs = Object.values(payload.data) as Champion[]
+            const champion = champs[0]
+            setChampion(champion as Champion)
+        })
     }
 
     const handleTextViewChange = useCallback(
@@ -36,7 +34,6 @@ export const ChampionSelector = () => {
         },
         [],
     );
-
 
     return (
         <ChampionView champion={champion} callback={handleTextViewChange} championsList={championsList}/>
