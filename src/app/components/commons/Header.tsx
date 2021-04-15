e654d53f0f1d1d728885e5e0e8bfc4f5f99c623e
RasussRogue <hechadi.mehdi.pro@gmail.com>
2021-04-15 15:39:47 +0200

    (SC-14) Added a title

--- src/app/components/commons/Header.tsx
+++ src/app/components/commons/Header.tsx
@@ -1,19 +1,20 @@
 import * as React from "react";
-import {AppBar, Button, createStyles, IconButton, SvgIcon, Theme, Toolbar} from "@material-ui/core";
+import {AppBar, Button, createStyles, IconButton, SvgIcon, Theme, Toolbar, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import HeaderIcon from "../../icons/header-icon.svg";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        root: {
-            flexGrow: 1,
+        typography: {
+            flexGrow: 0.85,
+            textAlign: 'center'
         },
         menuButton: {
             marginRight: theme.spacing(2),
         },
         appBar: {
-            marginBottom: '10px'
-        }
+            marginBottom: '10px',
+        },
     }),
 );
 
@@ -29,6 +30,7 @@ export const Header = () => {
                     </IconButton>
                     <Button href="/" color="inherit">Home</Button>
                     <Button href="/champion" color="inherit">Champions</Button>
+                    <Typography variant="h4" className={classes.typography}>Stat Check</Typography>
                 </Toolbar>
             </AppBar>
         </React.Fragment>
