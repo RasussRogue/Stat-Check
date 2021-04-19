4b9d494bd77176a0ff66db02f0dbac025e56742f
RasussRogue <hechadi.mehdi.pro@gmail.com>
2021-04-19 12:15:16 +0200

    (SC-15) Arranged the display of the model files

--- src/app/components/champion/default.ts
+++ src/app/components/champion/default.ts
@@ -4,47 +4,56 @@ export const defaultChampion = {
     title: 'the Destroyer',
     blurb: 'Placeholder',
     image: {
-        full:"Aatrox.png"
+        full: "Aatrox.png"
     },
     stats: {
-        hp:580,
-        mp:0,
-        movespeed:345,
-        armor:38,
-        attackdamage:60,
-        crit:0,
-        spellblock:32,
-        attackspeed:0.651
+        hp: 580,
+        mp: 0,
+        movespeed: 345,
+        armor: 38,
+        attackdamage: 60,
+        crit: 0,
+        spellblock: 32,
+        attackspeed: 0.651
     },
 
     passive: {
-        name:"Deathbringer Stance",
-        description:"Periodically, Aatrox's next basic attack deals bonus physical damage and heals him, based on the target's max health. ",
+        name: "Deathbringer Stance",
+        description: "Periodically, Aatrox's next basic attack deals bonus physical damage and heals him, based on the target's max health. ",
         image: {
-            full:"Aatrox_Passive.png"
+            full: "Aatrox_Passive.png"
         }
     },
 
-    spells:
-        [{name:"The Darkin Blade",
-        description:"Aatrox slams his greatsword down, dealing physical damage. He can swing three times, each with a different area of effect.",
-        image: {
-            full:"AatroxQ.png"
-        }},
-        {name:"Infernal Chains",
-            description:"Aatrox smashes the ground, dealing damage to the first enemy hit. Champions and large monsters have to leave the impact area quickly or they will be dragged to the center and take the damage again.",
+    spells: [
+        {
+            name: "The Darkin Blade",
+            description: "Aatrox slams his greatsword down, dealing physical damage. He can swing three times, each with a different area of effect.",
             image: {
-                full:"AatroxW.png"
-            }},
-        {name:"Umbral Dash",
-            description:"Passively, Aatrox heals when damaging enemy champions. On activation, he dashes in a direction.",
+                full: "AatroxQ.png"
+            }
+        },
+        {
+            name: "Infernal Chains",
+            description: "Aatrox smashes the ground, dealing damage to the first enemy hit. Champions and large monsters have to leave the impact area quickly or they will be dragged to the center and take the damage again.",
             image: {
-                full:"AatroxE.png"
-            }},
-        {name:"World Ender",
-            description:"Aatrox unleashes his demonic form, fearing nearby enemy minions and gaining attack damage, increased healing, and movement speed. If he gets a takedown, this effect is extended.",
+                full: "AatroxW.png"
+            }
+        },
+        {
+            name: "Umbral Dash",
+            description: "Passively, Aatrox heals when damaging enemy champions. On activation, he dashes in a direction.",
             image: {
-                full:"AatroxR.png"
-            }}]
+                full: "AatroxE.png"
+            }
+        },
+        {
+            name: "World Ender",
+            description: "Aatrox unleashes his demonic form, fearing nearby enemy minions and gaining attack damage, increased healing, and movement speed. If he gets a takedown, this effect is extended.",
+            image: {
+                full: "AatroxR.png"
+            }
+        }
+    ]
 }
 
--- src/app/components/model/models.ts
+++ src/app/components/model/models.ts
@@ -1,39 +1,39 @@
 export interface Data {
-    data:any
+    data: any
 }
 
 export interface Champion {
-    id:string
-    name:string,
-    title:string,
-    blurb:string
+    id: string
+    name: string,
+    title: string,
+    blurb: string
     image: {
-        full:string
+        full: string
     }
     stats: {
-        hp:Number,
-        mp:Number,
-        movespeed:Number,
-        armor:Number,
-        attackdamage:Number,
-        crit:Number,
-        spellblock:Number,
-        attackspeed:Number
+        hp: Number,
+        mp: Number,
+        movespeed: Number,
+        armor: Number,
+        attackdamage: Number,
+        crit: Number,
+        spellblock: Number,
+        attackspeed: Number
     }
     passive: {
-        name:string,
-        description:string,
+        name: string,
+        description: string,
         image: {
-            full:string
+            full: string
         }
     }
-    spells:Passive[]
+    spells: Passive[]
 }
 
 interface Passive {
-    name:string,
-    description:string,
+    name: string,
+    description: string,
     image: {
-        full:string
+        full: string
     }
 }
